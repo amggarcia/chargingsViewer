04134879a0017b169fdd1fefd17846b7341157dd
unknown <amg.garcia@hotmail.com>
2021-06-08 23:53:02 +0100

    Updates

---
+++ precache-manifest.03f6029cc35d3c93beff89b5014d7ad8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1d8bfb2d0356653eec9bfceb2bbaef",
+    "url": "/chargingsViewer/index.html"
+  },
+  {
+    "revision": "69eb2579bab174d6bf98",
+    "url": "/chargingsViewer/static/css/main.7ba44731.chunk.css"
+  },
+  {
+    "revision": "fe91b6c4fc1babfa6490",
+    "url": "/chargingsViewer/static/js/2.34e2421f.chunk.js"
+  },
+  {
+    "revision": "fad895a76b3b3277869e4a39bf51057a",
+    "url": "/chargingsViewer/static/js/2.34e2421f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69eb2579bab174d6bf98",
+    "url": "/chargingsViewer/static/js/main.bb0359d6.chunk.js"
+  },
+  {
+    "revision": "72d4f4bcee42b88df69b",
+    "url": "/chargingsViewer/static/js/runtime-main.89bf3a42.js"
+  }
+]);
